eabf12e3d5496787abea3adc544bb05f8fbc089b
avatarfreak <avatarfreak83@gmail.com>
2021-11-30 19:06:06 +0530

    Header component added

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+   return (
+      <header className="w-full min-h-[9.75rem] bg-mobile bg-no-repeat bg-pri-clr-100 bg-cover md:bg-desktop" >
+         <h1 className="sr-only">Static Job Listing</h1>
+      </header >
+   )
+}
+
+export { Header }
+
