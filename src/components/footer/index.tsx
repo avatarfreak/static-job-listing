16e9c7110b4664911d04155b116aa4def7729e39
avatarfreak <avatarfreak83@gmail.com>
2021-11-30 18:34:36 +0530

    Footer components updated

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const Footer = () => {
+   return (
+      <footer className="text-center text-xs">
+         Challenge by
+         <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-pri-clr-100" > Frontend Mentor
+         </a>
+         . Coded by
+         <a
+            href="https://www.github.com/avatarfreak"
+            target="_blank"
+            rel=" noopener noreferrer"
+            className="text-pri-clr-100"> avatarfreak
+         </a>
+      </footer>
+   )
+}
+
+export default Footer
+
