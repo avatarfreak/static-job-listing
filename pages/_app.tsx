e8940264389b8de36d8116ba229672eff97715ed
avatarfreak <avatarfreak83@gmail.com>
2021-11-30 18:25:16 +0530

    Added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import Layout from "@layout/index";
+import { JobProvider } from "context/JobProvider";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
@@ -5,15 +7,23 @@ import "tailwindcss/tailwind.css";
 import "../src/styles/index.scss";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-   return (
-      <>
-         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-         </Head>
-         <Component {...pageProps} />
-      </>
-   );
-}
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="static job listing challeges accepted by avatarfreak"
+        />
+        <title>Static Job Listing </title>
+      </Head>
+      <Layout>
+        <JobProvider>
+          <Component {...pageProps} />
+        </JobProvider>
+      </Layout>
+    </>
+  );
+};
 
 export default MyApp;
-
