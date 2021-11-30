dcd4436b2bcad28b21841beca86dcaafa574405e
avatarfreak <avatarfreak83@gmail.com>
2021-11-30 19:13:36 +0530

    base path updated

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-   trailingSlash: true,
-   webpackDevMiddleware: config => {
-      config.watchOptions = {
-         poll: 1000,
-         aggregateTimeout: 300,
-         ignored: /node_modules/
-      };
-      return config;
-   },
-   sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-   }
-}
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  trailingSlash: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+      ignored: /node_modules/,
+    };
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
