c490a969d8cb34099c5aa350903962ad0e4d74d8
avatarfreak <avatarfreak83@gmail.com>
2021-11-30 20:42:12 +0530

    h3 to h2 following semantic html

--- src/components/cards/Designation.tsx
+++ src/components/cards/Designation.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const Designation: React.FC<Props> = ({ position }) => {
   return (
-    <h3 className="text-fs-900 text-clr-130 font-bold tracking-t-0 lg:hover:text-pri-clr-100">
+    <h2 className="text-fs-900 text-clr-130 font-bold tracking-t-0 lg:hover:text-pri-clr-100">
       {position}
-    </h3>
+    </h2>
   );
 };
 
